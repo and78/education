0dda5a789627d37daab6ee95bd3e230d1da2ac07
Andres A <36209306+and78@users.noreply.github.com>
2023-05-21 20:01:35 -0500

    add eureka and apigateway modules

--- subjects/src/main/java/com/training/subjects/SubjectsApplication.java
+++ subjects/src/main/java/com/training/subjects/SubjectsApplication.java
@@ -4,7 +4,9 @@ import com.training.subjects.config.properties.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationProperties.class)
 public class SubjectsApplication {
--- users/src/main/java/com/training/user/UsersApplication.java
+++ users/src/main/java/com/training/user/UsersApplication.java
@@ -4,7 +4,9 @@ import com.training.user.config.properties.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationProperties.class)
 public class UsersApplication {
